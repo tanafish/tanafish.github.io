c1032e791e2e404cec7a6330ed7ac074d57fc121
Tana <102907101+tanafish@users.noreply.github.com>
2025-11-20 05:15:54 +0300

    Add files via upload

---
+++ display.php
@@ -0,0 +1,6 @@
+<?php
+$name = $_POST["firstname"];
+$surname = $_POST["lastname"];
+$namegruppa = $_POST["gruppa"];
+echo "Ваше имя, фамилия и группа: <b>".$name . " " .$surname . " " .$namegruppa . "</b>";
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+echo hex2bin('d0bfd180d0b8d0b2d0b5d182'); 
+phpinfo();
+?>
